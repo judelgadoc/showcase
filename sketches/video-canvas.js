95f4677a1134180bff29b9054d51e23ae46db30a
judelgadoc <judelgadoc@users.noreply.github.com>
2023-03-01 03:04:48 +0000

    deploy: 4a3585648b2f47dfa2b25d5fa3377c4123c45128

--- sketches/video-canvas.js
+++ sketches/video-canvas.js
@@ -1,9 +1,9 @@
 let fingers;
 
 function setup() {
-  createCanvas(710, 400);
+  createCanvas(windowWidth, windowHeight);
   // specify multiple formats for different browsers
-  fingers = createVideo(['/showcase/sketches/fingers.mov', '/showcase/sketches/fingers.webm']);
+  fingers = createVideo(['/visualcomputing/sketches/cute_little_moo.mp4']);
   fingers.hide(); // by default video shows up in separate dom
   // element. hide it and draw it to the canvas
   // instead
@@ -11,9 +11,7 @@ function setup() {
 
 function draw() {
   background(150);
-  image(fingers, 10, 10); // draw the video frame to canvas
-  filter(GRAY);
-  image(fingers, 150, 150); // draw a second copy to canvas
+  image(fingers, 0, 0, 0, height); // draw the video frame to canvas
 }
 
 function mousePressed() {
