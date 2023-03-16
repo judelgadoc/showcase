445ddd8ea2f22727e2f1de5cbef37bc9861114fd
judelgadoc <judelgadoc@users.noreply.github.com>
2023-03-16 04:48:00 +0000

    deploy: 4ec415af0cfadacadb2ecba579a013fbb17c28c6

--- sketches/ps001/ex08.js
+++ sketches/ps001/ex08.js
@@ -13,61 +13,50 @@ simply change the string in load to get to the file
 
 [1]: https://github.com/mfmyip/Pixelator
 */
-
-// playlists
-
 p5.disableFriendlyErrors = true;
 
 let obj;
 let pixelSize = 5;
 let toggle = 0; // 0: original, 1: spatial coherence, 2: average
 const modeText = ["Original", "Spatial C.", "Average"]
-let vW = 0; // video heigh and width, automatically set
+let vW = 0; // video height and width, automatically set
 let vH = 0;
 let load = "/visualcomputing/sketches/members/member01/cute_little_moo.mp4"
 let isMute = false;
 let isPause = false;
 
 function preload() {
-  obj = createVideo(load);
+    obj = createVideo(load);
 }
 
 function setup() {
-  createCanvas(720-24, 740-40);
-  vW = 720-24;
-  vH = 740-40;
-  obj.size(vW, vH)
-  noStroke();
-  background(0);
+    createCanvas(720 - 24, 740 - 40);
+    vW = 720 - 24;
+    vH = 740 - 40;
+    obj.size(vW, vH)
+    noStroke();
+    background(0);
     frameRate(10000);
-  obj.loop();
-  obj.hide();
+    obj.loop();
+    obj.hide();
 }
 
-/*
-getVideoDimensionsOf(load).then( ({width, height}) => {
-  vW = width;
-  vH = height;
-  reset();
-});
-*/
-
 function reset() {
-  createCanvas(vW, vH);
-  obj.loop();
-  obj.hide();
+    createCanvas(vW, vH);
+    obj.loop();
+    obj.hide();
 }
 
 
 function draw() {
     if (toggle === 0) {
-  image(obj, 0, 0);
-    textSize(50)
-    fill(255, 0, 0)
-    text(modeText[toggle], 10, 50)
-    text(round(frameRate()), 630, 50)
+        image(obj, 0, 0);
+        textSize(50)
+        fill(255, 0, 0)
+        text(modeText[toggle], 10, 50)
+        text(round(frameRate()), 630, 50)
     } else
-  Pixelate(0, 0, vW, vH, pixelSize);
+        Pixelate(0, 0, vW, vH, pixelSize);
     textSize(50)
     fill(255, 0, 0)
     text(modeText[toggle], 10, 50)
@@ -75,59 +64,61 @@ function draw() {
 }
 
 function keyPressed() {
-  if (key === 't' || key === 'T') {
-      toggle = (toggle + 1) % 3
-  }
-  if (key === ',') {
-    pixelSize = max(4, pixelSize - 2);
-  }
-  if (key === '.') {
-    pixelSize = min(50, pixelSize + 2);
-  }
-  if (key === 'm' || key === 'M') {
-    isMute = !isMute;
-    if (isMute) {
-      obj.volume(0);
-    } else {
-      obj.volume(1);
+    if (key === 't' || key === 'T') {
+        toggle = (toggle + 1) % 3
     }
-  }
-  if (key === 'p' || key === 'P') {
-    isPause = !isPause;
-    if (isPause) {
-      obj.pause();
-    } else {
-      obj.play();
+    if (key === ',') {
+        pixelSize = max(4, pixelSize - 2);
     }
-  }
-  if (key === ' ') {
-  	save("capture_pixel.jpg");
-  }
+    if (key === '.') {
+        pixelSize = min(50, pixelSize + 2);
+    }
+    if (key === 'm' || key === 'M') {
+        isMute = !isMute;
+        if (isMute) {
+            obj.volume(0);
+        } else {
+            obj.volume(1);
+        }
+    }
+    if (key === 'p' || key === 'P') {
+        isPause = !isPause;
+        if (isPause) {
+            obj.pause();
+        } else {
+            obj.play();
+        }
+    }
+    if (key === ' ') {
+        save("capture_pixel.jpg");
+    }
 }
 
 
 function Pixelate(startX, startY, endX, endY, pSize) {
-  obj.loadPixels();
-  for (let x = startX; x < endX; x += pixelSize) {
-    for (let y = startY; y < endY; y += pixelSize) {
-      let offset = ((y * vW) + x) * 4;
-        const average = (array) => array.reduce((a, b) => a + b) / array.length;
-    if(toggle === 1) {
-        fill(obj.pixels[offset], obj.pixels[offset + 1], obj.pixels[offset + 2]);
-    } else if (toggle === 2) {
-      fill(average([obj.pixels[offset-vW*4-4], obj.pixels[offset-vW*4], obj.pixels[offset-vW*4+4], 
-                    obj.pixels[offset-4], obj.pixels[offset], obj.pixels[offset+4],
-                    obj.pixels[offset+vW*4-4], obj.pixels[offset+vW*4], obj.pixels[offset+vW*4+4]]),
-           average([obj.pixels[offset-vW*4-4+1], obj.pixels[offset-vW*4+1], obj.pixels[offset-vW*4+4+1], 
-                    obj.pixels[offset-4+1], obj.pixels[offset+1], obj.pixels[offset+4+1],
-                    obj.pixels[offset+vW*4-4+1], obj.pixels[offset+vW*4+1], obj.pixels[offset+vW*4+4+1]]),
-           average([obj.pixels[offset-vW*4-4+2], obj.pixels[offset-vW*4+2], obj.pixels[offset-vW*4+4+2], 
-                    obj.pixels[offset-4+2], obj.pixels[offset+2], obj.pixels[offset+4+2],
-                    obj.pixels[offset+vW*4-4+2], obj.pixels[offset+vW*4+2], obj.pixels[offset+vW*4+4+2]])
-      );
+    obj.loadPixels();
+    for (let x = startX; x < endX; x += pixelSize) {
+        for (let y = startY; y < endY; y += pixelSize) {
+            let offset = ((y * vW) + x) * 4;
+            const average = (array) => array.reduce((a, b) => a + b) / array.length;
+            if (toggle === 1) {
+                fill(obj.pixels[offset], obj.pixels[offset + 1], obj.pixels[offset + 2]);
+            } else if (toggle === 2) {
+                fill(average([obj.pixels[offset - vW * 4 - 4], obj.pixels[offset - vW * 4], obj.pixels[offset - vW * 4 + 4],
+                        obj.pixels[offset - 4], obj.pixels[offset], obj.pixels[offset + 4],
+                        obj.pixels[offset + vW * 4 - 4], obj.pixels[offset + vW * 4], obj.pixels[offset + vW * 4 + 4]
+                    ]),
+                    average([obj.pixels[offset - vW * 4 - 4 + 1], obj.pixels[offset - vW * 4 + 1], obj.pixels[offset - vW * 4 + 4 + 1],
+                        obj.pixels[offset - 4 + 1], obj.pixels[offset + 1], obj.pixels[offset + 4 + 1],
+                        obj.pixels[offset + vW * 4 - 4 + 1], obj.pixels[offset + vW * 4 + 1], obj.pixels[offset + vW * 4 + 4 + 1]
+                    ]),
+                    average([obj.pixels[offset - vW * 4 - 4 + 2], obj.pixels[offset - vW * 4 + 2], obj.pixels[offset - vW * 4 + 4 + 2],
+                        obj.pixels[offset - 4 + 2], obj.pixels[offset + 2], obj.pixels[offset + 4 + 2],
+                        obj.pixels[offset + vW * 4 - 4 + 2], obj.pixels[offset + vW * 4 + 2], obj.pixels[offset + vW * 4 + 4 + 2]
+                    ])
+                );
+            }
+            rect(x, y, pSize, pSize);
+        }
     }
-      rect(x, y, pSize, pSize);
-    }
-  }
 }
-
